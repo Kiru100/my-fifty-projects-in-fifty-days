4c659e76e95f61e74dd0fe4ea835e8ac64bdcfe2
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-04-24 20:00:22 +0800

    flexbox nyan cat project

--- FAQ Boxes/script.js
+++ FAQ Boxes/script.js
@@ -2,6 +2,7 @@ const btn=document.querySelectorAll('.faq-toggle');
 
 btn.forEach( toggle =>{
     toggle.addEventListener('click', () => {
+        
         toggle.parentNode.classList.toggle('active')
     })
 })
---
+++ Flexbox Home Demonstration/app.js
@@ -0,0 +1,10 @@
+const btn=document.querySelectorAll('.box');
+
+btn.forEach(toggle =>{
+    toggle.addEventListener('click', ()=>{
+           toggle.classList.toggle('active')
+
+
+        console.log(gege)
+    })
+})
