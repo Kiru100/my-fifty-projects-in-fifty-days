51ef754c7e5f4a10c16d37d796687776bd25514f
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-06-08 01:43:37 +0800

    Create script.js

---
+++ Toast Notification/script.js
@@ -0,0 +1,29 @@
+const showNotif = document.getElementById('button') 
+const toasts = document.getElementById('toasts')
+
+
+const toastMessage = ['Pang una', 'Pangalawa', 'Pangatlo', 'Pang apat', 'Pang Lima'] 
+
+showNotif.addEventListener('click', ()=>labasnotif())
+
+
+function labasnotif (){
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+
+    notif.innerText=getRandomNumber();
+
+
+
+    toasts.appendChild(notif)
+
+    setTimeout(()=>{
+        notif.remove()
+    },3000)
+
+  
+}
+
+function getRandomNumber(){
+    return toastMessage[Math.floor(Math.random() * toastMessage.length)]
+}
