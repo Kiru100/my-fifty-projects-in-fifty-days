493133b1305c9e437967e1e0ce2b37fefbd6cfec
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-06-23 14:31:51 +0800

    Update script.js

--- Double Heart Click/script.js
+++ Double Heart Click/script.js
@@ -21,25 +21,25 @@ loveMe.addEventListener('click', (e) => {
 })
 
 const createHeart = (e) =>{
- const heart = document.createElement('i')
- heart.classList.add('fas')
- heart.classList.add('fa-heart')
- const x = e.clientX
- const y = e.clientY
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+    const x = e.clientX
+    const y = e.clientY
 
- const leftOffset = e.target.offsetLeft
- const topOffset = e.target.offsetTop
- 
- const xInside = x - leftOffset
- const yInside = y - topOffset
- 
- heart.style.top =`${yInside}px`
- heart.style.left =`${xInside}px`
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+    
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+    
+    heart.style.top =`${yInside}px`
+    heart.style.left =`${xInside}px`
 
- loveMe.appendChild(heart);
- times.innerHTML = ++timesClicked;
+    loveMe.appendChild(heart);
+    times.innerHTML = ++timesClicked;
 
- setTimeout(() => heart.remove(), 1000);
+    setTimeout(() => heart.remove(), 1000);
 }
 
 
