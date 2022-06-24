a75d9de3869a32b1f607e96ee33e9fa88c73d346
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-06-24 19:15:03 +0800

    Create script.js

---
+++ Auto Text Effect/script.js
@@ -0,0 +1,21 @@
+const textElement = document.getElementById('text')
+const speedElement = document.getElementById('speed')
+const text = 'I am Noel M. Dasco Jr.'
+
+let idx = 1;
+let speed = 300 / speedElement.value
+
+writeText()
+
+function writeText(){
+    textElement.innerHTML = text.slice(0,idx)
+    idx++;
+
+    if(idx> text.length){
+        idx=1;
+    }
+
+    setTimeout(writeText, speed)
+}
+
+speedElement.addEventListener('input', (e)=> speed = 300 / e.target.value)
