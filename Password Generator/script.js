09624c94b79faf3d3d063f894af00f181f712868
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-09-02 16:02:44 +0800

    Update script.js

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,9 +1,34 @@
+const resultEl = document.getElementById('result');
+const lengthtEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+
 const randomFunction = {
     lower: getRandomLower,
     upper:getRandomUpper,
     number:getRandomNumber,
     symbol:getRandomSymbol
 }
+
+generateEl.addEventListener('click', () =>{
+    const length = +lengthtEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+    
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+clipboardEl.addEventListener('click', ()=>{
+    document.execCommand('copy');
+});
+
 
 function getRandomLower(){
     return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97)
@@ -12,7 +37,6 @@ function getRandomLower(){
 function getRandomUpper(){
     return String.fromCharCode(Math.floor(Math.random() * 26 ) + 65)
 }
-
 
 function getRandomNumber(){
     return String.fromCharCode(Math.floor(Math.random() * 10 ) + 48)
@@ -24,5 +48,23 @@ function getRandomSymbol(){
     return symbols[number];
 }
 
+function generatePassword(lower, upper, number, symbol, length){
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typeArray = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
 
-console.log(getRandomSymbol())
+    if(typesCount === 0){
+        return '';
+    }
+
+    for(let i=0; i<length; i+=typesCount){
+        typeArray.forEach(type =>{
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunction[funcName]();
+            console.log(generatedPassword);
+        });
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+}
