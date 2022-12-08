0701ecd39e000232b5ab61f680961ca264779c1f
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-12-08 22:51:57 +0800

    Update script.js

--- Notes App/script.js
+++ Notes App/script.js
@@ -52,11 +52,11 @@ function addNewNote(text = ''){
     document.body.appendChild(note);
 }
 
+
 function updateLS(){
     const notesText = document.querySelectorAll('textarea');
     const notes = [];
 
     notesText.forEach(note => notes.push(note.value));
-    
     localStorage.setItem('notes', JSON.stringify(notes));
 }
