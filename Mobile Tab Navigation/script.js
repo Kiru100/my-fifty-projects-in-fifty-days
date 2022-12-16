b295869a9e785a81c482838e52551beea4e98807
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-12-16 23:35:23 +0800

    Update script.js

--- Mobile Tab Navigation/script.js
+++ Mobile Tab Navigation/script.js
@@ -3,15 +3,15 @@ const buttons = document.querySelectorAll('ul li');
 
 
 for(let index = 0; index < buttons.length; index++){
-    buttons[index].addEventListener('click', (event)=>{clickedMenu(index, buttons[index])});
+    buttons[index].addEventListener('click',()=>{clickedMenu(index, buttons[index])});
 }
 
-
 const clickedMenu = (index) =>{
-    for(let index=0; index<buttons.length; index++){
+    for(let index = 0; index<buttons.length; index++){
         buttons[index].classList.remove('active');
         images[index].classList.remove('show');
     }
+
     buttons[index].classList.add('active');
     images[index].classList.add('show');
 }
